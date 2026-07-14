3b4ee837b3d5f996339c0def0b9a063a72ac6051
Arti Sharma <sharmaarti2486@gmail.com>
2026-07-14 09:20:25 +0530

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AI_Makeup_Artist/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
